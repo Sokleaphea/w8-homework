f5753896a368f4b71b0d594a4eed6c17586ce484
phea <sokleapheaphon@gmail.com>
2026-03-11 22:46:12 +0700

    finished

---
+++ lib/data/repositories/songs/song_repository_mock.dart
@@ -0,0 +1,67 @@
+// song_repository_mock.dart
+
+import '../../../model/songs/song.dart';
+import 'song_repository.dart';
+
+class SongRepositoryMock implements SongRepository {
+  int _fetchCounts = 0;
+  final List<Song> _songs = [
+    Song(
+      id: 's1',
+      title: 'Mock Song 1',
+      artist: 'Mock Artist',
+      duration: const Duration(minutes: 2, seconds: 50),
+    ),
+    Song(
+      id: 's2',
+      title: 'Mock Song 2',
+      artist: 'Mock Artist',
+      duration: const Duration(minutes: 3, seconds: 20),
+    ),
+    Song(
+      id: 's3',
+      title: 'Mock Song 3',
+      artist: 'Mock Artist',
+      duration: const Duration(minutes: 3, seconds: 20),
+    ),
+    Song(
+      id: 's4',
+      title: 'Mock Song 4',
+      artist: 'Mock Artist',
+      duration: const Duration(minutes: 3, seconds: 20),
+    ),
+    Song(
+      id: 's5',
+      title: 'Mock Song 5',
+      artist: 'Mock Artist',
+      duration: const Duration(minutes: 3, seconds: 20),
+    ),
+  ];
+
+  @override
+  Future<List<Song>> fetchSongs() async {
+    _fetchCounts++;
+    await Future.delayed(Duration(seconds: 2));
+
+    if (_fetchCounts % 2 == 0) {
+      throw Exception("Simulated network error");
+    }
+
+    return _songs;
+  }
+
+  @override
+  Future<Song?> fetchSongById(String id) async {
+    // - Simulate a delay of 3 seconds.
+    await Future.delayed(Duration(seconds: 3));
+    // - After the delay : Find the song of given id in the list of songs and return it
+    final song = _songs.firstWhere(
+      (s) => s.id == id,
+      orElse: () => throw Exception("no song found for id $id in the database"),
+    );
+
+    // - If not found : Throw an error with the message “no song found for id 25 in the database"
+
+    return song; // TO CHANGE !
+  }
+}
